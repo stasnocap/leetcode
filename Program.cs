84209f1e02ef61feaf5a61b60e173ab53c5c955a
Стас <stasnocap@gmail.com>
2023-12-27 17:53:06 +0300

    27. Remove Element in process

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var ints = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+var result = RemoveElement(ints, 2);
+Console.WriteLine(result);
+
+int RemoveElement(int[] nums, int val)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int j = nums.Length - 1;
+        int temp = 0;
+        while (temp == val && val > 0 && j > 0)
+        {
+            temp = nums[j--];
+            nums[i] = ;
+        }
+    }
+
+    return nums.Length;
+}
